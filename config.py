acec3107b6a9f4ff35e97a75246bace17d7ad2ca
unknown <msaste3@gmail.com>
2022-10-22 22:39:10 +0530

    .

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = 'enter_path'
+
+PORT_NUMBER = 5007
+
+MODEL_FILE_PATH = r'E:\Data_sets\vehicleset2\project_data\Linear_Model.pkl'
+
+JSON_FILE_PATH = r'E:\Data_sets\vehicleset2\project_data\Label_Encoded_columns.json'
